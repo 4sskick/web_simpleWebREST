d13dcffecc6bca9cb759876c5b54308d454505a2
Septian Adi Wijaya <developadi@gmail.com>
2020-12-02 16:10:07 +0700

    [API] get - greeting with params

---
+++ src/main/java/com/niteroomcreation/simplewebrest/controllers/GreetingController.java
@@ -0,0 +1,34 @@
+package com.niteroomcreation.simplewebrest.controllers;
+
+import com.niteroomcreation.simplewebrest.models.Greeting;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+/**
+ * Created by monta on 02/12/20
+ * please make sure to use credit when you're using people's code
+ */
+
+@RestController
+@RequestMapping("/api")
+public class GreetingController {
+
+    private final AtomicLong counter = new AtomicLong();
+
+    @GetMapping("/greetings")
+    public ResponseEntity<String> greeting() {
+        return ResponseEntity.ok("Hello from java Spring Web");
+    }
+
+    //would returning as JSON content
+    //default is using Jackson 2 to be parsed
+    @GetMapping("/greeting")
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "USER") String name) {
+        return new Greeting(counter.incrementAndGet(), String.format("Hello %s, welcome to java Spring Web", name));
+    }
+}
---
+++ src/main/java/com/niteroomcreation/simplewebrest/models/Greeting.java
@@ -0,0 +1,40 @@
+package com.niteroomcreation.simplewebrest.models;
+
+/**
+ * Created by monta on 02/12/20
+ * please make sure to use credit when you're using people's code
+ */
+public class Greeting {
+
+    private long id;
+    private String content;
+
+    public Greeting(long id, String content){
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    @Override
+    public String toString() {
+        return "Greeting{" +
+                "id=" + id +
+                ", content='" + content + '\'' +
+                '}';
+    }
+}
