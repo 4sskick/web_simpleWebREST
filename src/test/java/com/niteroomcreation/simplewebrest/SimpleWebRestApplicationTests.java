5a74b57e1a685c7029f38575dd446e8cda03a797
Septian Adi Wijaya <developadi@gmail.com>
2020-12-02 16:26:42 +0700

    [jUnit] greeting

--- src/test/java/com/niteroomcreation/simplewebrest/SimpleWebRestApplicationTests.java
+++ src/test/java/com/niteroomcreation/simplewebrest/SimpleWebRestApplicationTests.java
@@ -1,13 +1,20 @@
 package com.niteroomcreation.simplewebrest;
 
+import com.niteroomcreation.simplewebrest.controllers.GreetingController;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SimpleWebRestApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private GreetingController greetingController;
+
+    @Test
+    void contextLoads() {
+        Assertions.assertTrue(greetingController != null);
+    }
 
 }
