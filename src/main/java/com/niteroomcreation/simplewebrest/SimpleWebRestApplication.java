4f6b68632f376127fafc17ad90d8eded9405c433
Septian Adi Wijaya <developadi@gmail.com>
2020-12-02 15:30:31 +0700

    init setup

---
+++ src/main/java/com/niteroomcreation/simplewebrest/SimpleWebRestApplication.java
@@ -0,0 +1,13 @@
+package com.niteroomcreation.simplewebrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleWebRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleWebRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/niteroomcreation/simplewebrest/SimpleWebRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.niteroomcreation.simplewebrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleWebRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
