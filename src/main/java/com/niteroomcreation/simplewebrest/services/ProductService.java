c0e015d85740b088357499cee0a6cf0135077268
Septian Adi Wijaya <developadi@gmail.com>
2020-12-04 17:15:23 +0700

    [repository] product service

---
+++ src/main/java/com/niteroomcreation/simplewebrest/services/ProductService.java
@@ -0,0 +1,37 @@
+package com.niteroomcreation.simplewebrest.services;
+
+import com.niteroomcreation.simplewebrest.data.repositories.ProductRepository;
+import com.niteroomcreation.simplewebrest.models.Product;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * Created by monta on 04/12/20
+ * please make sure to use credit when you're using people's code
+ */
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepository repo;
+
+    //operation DB
+    public List<Product> getAll(){
+        return repo.findAll();
+    }
+
+    public void save(Product model){
+        repo.save(model);
+    }
+
+    public Product getById(Integer id){
+        return repo.findById(id).get();
+    }
+
+    public void deleteById(Integer id){
+        repo.deleteById(id);
+    }
+}
